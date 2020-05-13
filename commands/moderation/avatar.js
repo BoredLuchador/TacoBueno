2e4b1ad32137f89dfb40a32d5b672d759e3ee0df
PrimoPunch <63678426+PrimoPunch@users.noreply.github.com>
2020-05-12 21:16:53 -0500

    added avatar command

--- commands/moderation/avatar.js
+++ commands/moderation/avatar.js
@@ -2,12 +2,24 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "avatar",
-    aliases: "pfp",
+    aliases: ["pfp"],
     category: "moderation",
     description: "Returns avatar image in png format",
     run: async (clinet, message, args) => {
 
-        message.channel.send(message.author.displayAvatar);
+        let image = message.author.displayAvatarURL({ format: "png", dynamic: true});
+        let name = message.author;
+        
+        const des = [ name, '`s profile image'];
+
+        const Embed = new Discord.MessageEmbed() //Embed stuff here//
+                .setColor("RANDOM")
+                .setTitle('Image link')
+                .setURL(image)
+                .setDescription(des.join(" "))
+                .setImage(image)
+                .setTimestamp()
+            message.channel.send(Embed);
 
     }
 }
