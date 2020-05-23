a147f47d5a568471c79a1eef3f601dffef5ac9df
PrimoPunch <63678426+PrimoPunch@users.noreply.github.com>
2020-05-22 21:36:15 -0500

    Added module.exports command

---
+++ commands/98-dev-Tools/module.exports.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "module.exports",
+    category: "Dev-Tools",
+    description: "Shows proper command modue structure",
+    run: async (client, message, args) => {
+
+        const Moduleexport = new Discord.MessageEmbed()
+            .setColor('#44A290')
+            .setTitle('module.exports format')
+            .setDescription('module.exports = {\n   name:"",\n  aliases:"",\n   category: "",\n     usage:"",\n   run: async (client, message, args) => {\n}\n}')
+            .setTimestamp();
+
+            message.channel.send(Moduleexport)
+
+    }
+}
