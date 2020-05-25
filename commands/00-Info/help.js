00b8df69edd01a735664971fb784811cba7d5029
PrimoPunch <63678426+PrimoPunch@users.noreply.github.com>
2020-05-25 12:59:35 -0500

    added hard-coded help

--- commands/00-Info/help.js
+++ commands/00-Info/help.js
@@ -8,12 +8,18 @@ module.exports = {
     description: "Shows a list of command, or details on one command",
     usage: "[command | alias of command]",
     run: async (client, message, args) => {
-
-    }
-}
-
-function getAll(client, message) {
-    const embed = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-
+ 
+const embed = new Discord.MessageEmbed()
+     .setColor('#6A8372')
+     .setTitle('Help Panel')
+     .setDescription("*Note that this help command is currently hardcoded and will not be as up to date. If that is the case, please let the developer know.*")
+     .addFields (
+         { name: 'Info', value: '`help` `ping`', inline: true},
+         { name: 'Moderation', value: '`avatar` `report` `say`', inline: true},
+         { name: 'Silly Commands', value: '`confess`', inline: true},
+         { name: 'Dev tools', value: '`bin` `module.exports`', inline: true},
+         { name: 'Broken commands', value: '`whois`', inline: true},
+     )
+     message.channel.send(embed);
+     }
 }
