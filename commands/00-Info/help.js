3c4aacca06c0e364b711d17a308d9bd14f91056c
PrimoPunch <63678426+PrimoPunch@users.noreply.github.com>
2020-05-28 19:45:54 -0500

    improved the help command to be more readable

--- commands/00-Info/help.js
+++ commands/00-Info/help.js
@@ -14,11 +14,11 @@ const embed = new Discord.MessageEmbed()
      .setTitle('Help Panel')
      .setDescription("*Note that this help command is currently hardcoded and will not be as up to date. If that is the case, please let the developer know.*")
      .addFields (
-         { name: 'Info', value: '`help` `ping`', inline: true},
-         { name: 'Moderation', value: '`avatar` `report` `say`', inline: true},
-         { name: 'Silly Commands', value: '`confess`', inline: true},
-         { name: 'Dev tools', value: '`bin` `module.exports`', inline: true},
-         { name: 'Broken commands', value: '`whois`', inline: true},
+         { name: '**Info**', value: '`help`\n`ping`', inline: true},
+         { name: '**Moderation**', value: '`avatar`\n`report`\n`say`', inline: true},
+         { name: '**Silly Commands**', value: '`confess`', inline: true},
+         { name: '**Dev tools**', value: '`bin`\n`module.exports`', inline: true},
+         { name: '**Broken commands**', value: '`whois`', inline: true},
      )
      message.channel.send(embed);
      }
