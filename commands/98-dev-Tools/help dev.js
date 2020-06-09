9738f4e303b82e361ec47543b149471e2b99088f
PrimoPunch <63678426+PrimoPunch@users.noreply.github.com>
2020-06-09 18:23:07 -0500

    added sources

--- commands/98-dev-Tools/help dev.js
+++ commands/98-dev-Tools/help dev.js
@@ -17,7 +17,7 @@ module.exports = {
 			.setDescription('instead of bookmarking all of the links, you instead made a command that you hope that nobody else finds where you list all of the things you need to look at often. noice')
 			.addFields(
 				{ name: 'Basic Discord js links (v12)', value: '[Official docs](https://discord.js.org/#/docs/main/12.2.0/general/welcome), [Official Guide](https://discordjs.guide/)' },
-				{ name: 'Random things you should\'ve learned lmao', value: '[Folder stuff](https://estk.aenhancers.com/3%20-%20File%20System%20Access/using-file-and-folder-objects.html), [Mozilla docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript)' },
+				{ name: 'Random things you should\'ve learned lmao', value: '[Folder stuff](https://estk.aenhancers.com/3%20-%20File%20System%20Access/using-file-and-folder-objects.html), [Mozilla docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript), [Hosting with heroku](https://youtu.be/6By-t-FaKPc)' },
 				{ name: 'Youtube channels that give good ideas', value: '[The Source code](https://www.youtube.com/channel/UCNXt2MrZaqfIBknamqwzeXA), [Stuy](https://www.youtube.com/user/DatAdam93/featured), [Menu Docs](https://www.youtube.com/channel/UCpGGFqJP9vYvzFudqnQ-6IA/featured), [Salvage_Dev](https://www.youtube.com/channel/UC7-pjRSGoNEMoIujwOH2Mhw/featured)' },
 			);
 		message.channel.send(devembed);
