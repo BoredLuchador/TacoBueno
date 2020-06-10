8e93c984e99f58bfc4db75a26154f74da541f11a
PrimoPunch <63678426+PrimoPunch@users.noreply.github.com>
2020-06-10 16:11:13 -0500

    save point

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 const fs = require('fs');
 const { Client, Collection } = require('discord.js');
 const { config } = require('dotenv');
-const {
-	prefix,
-	token,
-} = require('./config.json');
+const { prefix } = require('./config.json');
 
 // Setup //
 const client = new Client();
@@ -29,30 +26,37 @@ config({
 // Stuff below will be replaced by EVENT HANDLER//
 // READY EVENT//
 
-client.on('ready', () => {
+client.once('ready', () => {
 	console.log(`I am now online, my name is ${client.user.username}`);
 
 	client.user.setPresence({
 		// Color Dot Status//
-		status: 'idle',
-		game: {
-			// Game Profile//
-			name: 'How do I work this',
-			type: 'PLAYING',
-		},
+		status: 'online',
 	});
+	client.user.setActivity('Primo Fail at coding', { type: 'WATCHING' });
 });
 
 // message event in console//
-client.on('message', async message => {
+client.on('message', async (message) => {
 
+	// Sends prefix by mentioning the bot. also uses mentioning the bot as the prefix
+	const mentionRegex = RegExp(`^<@!${client.user.id}>$`);
+	const mentionRegexPrefix = RegExp(`^<@!${client.user.id}> `);
+
+	if (!message.guild || message.author.bot) return;
+
+	if (message.content.match(mentionRegex)) message.channel.send(`My prefix for this server is \`${prefix}\`.`);
+
+	// Sets the prefix const using the mention and falling back to actual prefix
+
+	const dprefix = message.content.match(mentionRegexPrefix) ?
+		message.content.match(mentionRegexPrefix)[0] : prefix;
+
+	// CONDITION ? TRUE : FALSE
+	// Creating the command and args array //
+	const [cmd, ...args] = message.content.slice(dprefix.length).trim().split(/ +/g);
 
 	// Checks to prevent random bad stuff from happening/
-
-	if (!message.content.startsWith(prefix) || !message.guild || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(' ');
-	const cmd = args.shift().toLowerCase();
 
 	if (cmd.length === 0) return;
 
@@ -75,4 +79,4 @@ client.on('debug', (e) => console.info(e));
 
 
 // Logging the bot in//
-client.login(token);
+client.login(process.env.TOKEN);
