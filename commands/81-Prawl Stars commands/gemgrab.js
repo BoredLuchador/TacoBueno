6e72f6a8a0e77be47b87ad47e6a076fc5052ae94
Primo <you@example.com>
2021-01-13 00:31:51 -0600

    simplifed showdown.js and gemgrab.js

--- commands/81-Prawl Stars commands/gemgrab.js
+++ commands/81-Prawl Stars commands/gemgrab.js
@@ -17,34 +17,37 @@ module.exports = {
 		let gemcountdown = 0;
 		let reverse = Math.floor(Math.random() * 4 + 1);
 		let elprimo = Math.floor(Math.random() * 12 + 1);
-		message.channel.send('You started a game of Gem Grab!');
+
+		const data = [];
+
+		data.push('You started a game of Gem Grab!');
 		if (gem1 === 1) {
-			message.channel.send(
+			data.push(
 				'You failed to rush the generator, the enemies have the start',
 			);
 		}
-		else {message.channel.send('You rushed the gem generator and have a head start');}
+		else {data.push('You rushed the gem generator and have a head start');}
 		if (gem1 == 2 || gem1 == 3) {
 			if (gem2 === 1) {
-				message.channel.send(
+				data.push(
 					'The teammate with the gems died, and the enemies took them',
 				);
 			}
 			if (gem2 === 2) {
-				message.channel.send(
+				data.push(
 					'The teammate with the gems died, but you got them back',
 				);
 			}
 		}
 		if (gem2 === 1) {
 			if (gem3a === 1) {
-				message.channel.send(
+				data.push(
 					'The enemies hid in a bush and took down the gem carrier',
 				);
 			}
 			if (gem3a === 1) {gemcountdown = 2;}
 			else {
-				message.channel.send(
+				data.push(
 					'You grinded gems at the generator, defending from the enemies',
 				);
 			}
@@ -52,31 +55,32 @@ module.exports = {
 		}
 		if (gem2 == 2 || gem2 == 3) {
 			if (gem3b == 1 || gem3b == 3) {
-				message.channel.send(
+				data.push(
 					'The enemies hid in a bush and took down the gem carrier',
 				);
 			}
 			if (gem3b == 1 || gem3b == 3) {gemcountdown = 2;}
 			else {
-				message.channel.send(
+				data.push(
 					'You grinded gems at the generator, defending from the enemies',
 				);
 			}
 			if (gem3b === 2) gemcountdown = 1;
 		}
-		message.channel.send('Countdown!');
+		data.push('Countdown!');
 		if (gemcountdown === 1) {
-			if (reverse === 1) {message.channel.send('The enemy team killed the gem carrier. THEY WIN!');}
+			if (reverse === 1) {data.push('The enemy team killed the gem carrier. THEY WIN!');}
 			else if (elprimo === 1) {
-				message.channel.send(
+				data.push(
 					'What is that? EL PRIMO JUMPED WITH THE GEMS! **THEY WIN!**',
 				);
 			}
-			else {message.channel.send('YOU WIN!');}
+			else {data.push('YOU WIN!');}
 		}
 		if (gemcountdown === 2) {
-			if (reverse === 1) {message.channel.send('Your team killed the enemy gem carrier. YOU WIN!');}
-			else {message.channel.send('THEY WIN!');}
+			if (reverse === 1) {data.push('Your team killed the enemy gem carrier. YOU WIN!');}
+			else {data.push('THEY WIN!');}
 		}
+		message.channel.send(data, { split: true });
 	},
 };
--- commands/81-Prawl Stars commands/showdown.js
+++ commands/81-Prawl Stars commands/showdown.js
@@ -18,16 +18,19 @@ module.exports = {
 		let action1a = null;
 		let action1b = null;
 
+		// data table
+		const data = [];
+
 		if (action1 === 1) {
-			message.channel.send(
-				'You have gone for the middle! You have ' +
+			data.push(
+				'You have gone for the middle! \nYou have ' +
             Math.floor(Math.random() * 4 + 2) +
-            ' powercubes',
+            ' powercubes.',
 			);
 		}
 		if (action1 === 2) {
-			message.channel.send(
-				'You have gone for boxes at the edge of the map. You got ' +
+			data.push(
+				'You have gone for boxes at the edge of the map. \nYou got ' +
             Math.floor(Math.random() * 3 + 1) +
             ' powercubes.',
 			);
@@ -38,97 +41,101 @@ module.exports = {
 		if (action1 === 2) {
 			action1b = Math.floor(Math.random() * 3 + 1);
 		}
+
+
 		if (action1a === 1) {
-			message.channel.send(
-				'You were killed by the people at middle. you get 10th place.',
+			data.push(
+				'You were killed by the people at middle. you get `10th` place.',
 			);
 		}
 		if (action1a === 2) {
-			message.channel.send(
-				'You were killed by the people at middle. you get 9th place.',
+			data.push(
+				'You were killed by the people at middle. you get `9th` place.',
 			);
 		}
-		if (action1a === 1) return;
-		if (action1a === 2) return;
-		if (action1a === 3) {message.channel.send('You dominated the middle, and killed everyone there');}
-		if (action1b === 1) message.channel.send('You died. You get 8th place');
+		if (action1a === 1 || action1a === 2) return message.channel.send(data, { split: true });
+
+		if (action1a === 3) {data.push('You dominated the middle, and killed everyone there.');}
+		if (action1b === 1) data.push('You died. You get 8th place.');
 		if (action1b === 1) return;
 		if (action1b === 2) {
-			message.channel.send(
-				'You have got recurces, and gone to fight people at middle',
+			data.push(
+				'You have got recurces, and gone to fight people at middle.',
 			);
 		}
 		if (action1b === 3) {
 			message.channel.send(
-				'You have got recurces, and gone to fight people at middle',
+				'You have got recurces, and gone to fight people at middle.',
 			);
 		}
-		if (action2 === 1) {message.channel.send('A meteor killed you. You get 7th place.');}
-		if (action2 === 1) return;
-		if (action2 === 2) {message.channel.send('You are now at the middle and you killed a brawler.');}
-		if (action2 === 3) {message.channel.send('You are now at the middle and you killed a brawler.');}
-		if (action2 === 4) {message.channel.send('You are now at the middle and you killed a brawler.');}
+		if (action2 === 1) {data.push('A meteor killed you. You get 7th place.');}
+		if (action2 === 1) return message.channel.send(data, { split: true });
+		if (action2 === 2) {data.push('You are now at the middle and you killed a brawler.');}
+		if (action2 === 3) {data.push('You are now at the middle and you killed a brawler.');}
+		if (action2 === 4) {data.push('You are now at the middle and you killed a brawler.');}
 		if (action3 === 1) {
-			message.channel.send(
-				'You have gone camping into a bush, and killed the Nita that was there',
+			data.push(
+				'You have gone camping into a bush, and killed the Nita that was there.',
 			);
 		}
 		if (action3 === 2) {
-			message.channel.send(
+			data.push(
 				'You have gone camping into a bush, But you were killed by the Bull in there. You get 6th place.',
 			);
 		}
 		if (action3 === 3) {
-			message.channel.send(
+			data.push(
 				'You have gone camping into a bush, But you were killed by the Rosa in there. You get 5th place.',
 			);
 		}
-		if (action3 === 2) return;
-		if (action3 === 3) return;
+		if (action3 === 2 || action3 === 3) return message.channel.send(data, { split: true });
+
+
 		if (action3 === 4) {
-			message.channel.send(
-				'You have gone exploring for the brawlers left, and you killed a Bea',
+			data.push(
+				'You have gone exploring for the brawlers left, and you killed a Bea.',
 			);
 		}
 		if (action3 === 5) {
-			message.channel.send(
-				'You have gone exploriring around the map, and killed a Rico',
+			data.push(
+				'You have gone exploring around the map, and killed a Rico.',
 			);
 		}
 		if (action4 === 1) {
-			message.channel.send(
-				'You have been rushed on when you were low health. You get 3rd place.',
+			data.push(
+				'You have been rushed on when you were low health. You get `3rd` place.',
 			);
 		}
 		if (action4 === 2) {
-			message.channel.send(
-				'You have been rushed on by teamers. You get 4th place.',
+			data.push(
+				'You have been rushed on by teamers. You get `4th` place.',
 			);
 		}
 		if (action4 === 3) {
-			message.channel.send(
+			data.push(
 				'You killed the rest of the people wits ease. ***YOU WIN!***',
 			);
 		}
 		if (action4 === 4) {
-			message.channel.send(
-				'You have been rushed on when you were low health. You get 3rd place.',
+			data.push(
+				'You have been rushed on when you were low health. You get `3rd` place.',
 			);
 		}
 		if (action4 === 5) {
-			message.channel.send(
-				'You have been rushed on by teamers. You get 4th place.',
+			data.push(
+				'You have been rushed on by teamers. You get `4th` place.',
 			);
 		}
 		if (action4 === 6) {
-			message.channel.send(
-				'You killed the rest of the people wits ease. ***YOU WIN!***',
+			data.push(
+				'You killed the rest of the people with ease. ***YOU WIN!***',
 			);
 		}
 		if (action4 === 7) {
-			message.channel.send(
-				'The last brawler alive managed to kill you. You get 2nd place.',
+			data.push(
+				'The last brawler alive managed to kill you. You get `2nd` place.',
 			);
 		}
+		message.channel.send(data, { split: true });
 	},
 };
