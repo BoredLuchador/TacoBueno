0a2aacd3eb06b7abc9ba6998c8b8508952dd02f1
El Preemo <Elprimothethiccgod72648@gmail.com>
2021-05-09 20:14:27 -0500

    unban command in progress
    The command will be unusable for quite a while untill i figure out how to make it properly work

--- commands/10-Moderation/unban.js
+++ commands/10-Moderation/unban.js
@@ -1,52 +1,41 @@
 const { MessageEmbed } = require('discord.js');
-
+// Currently broken DO NOT USE
 module.exports = {
-	name: 'unban',
+	name: 'unban1',
 	aliases: false,
 	category: '10',
-	description: 'Unans someone that was previously banned from the server',
+	description: 'Unbans someone that was previously banned from the server',
 	cooldown : false,
 	guildOnly: true,
 	NSFW: false,
 	args: true,
-	usage: '<ping> [reason]',
+	usage: '<id> [reason]',
 	run: async (client, message, args) => {
 		if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You can\'t use that!');
 		if(!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send('I don\'t have the right permissions.');
 
-		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-
-		if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
-		if(!member.deleted) return message.channel.send('This user can\'t be unbanned. They are defintely not banned');
-
-		if(member.id === message.author.id) return message.channel.send('Can you explain to me how you will unaban yourself from a server that you are texting in? ');
+		if(args[0] === message.author.id) return message.channel.send('Can you explain to me how you will unaban yourself from a server that you are texting in? ');
 
 		let reason = args.slice(1).join(' ');
-
 		if(reason === undefined) reason = 'Unspecified';
 
-		member.unban(reason)
-			.catch(err => {
-				if(err) return message.channel.send('Something went wrong');
-			});
+		let member = client.users.resolveID(args[0]);
+
+		message.guild.fetchBan(member).then(user => {
+			message.guild.members.unban(`${user}}, ${reason}`)
+				.catch(err => {
+					if(err) return message.channel.send(`Something went wrong ${err}`);
+				});
+		});
 
 		const unbanembed = new MessageEmbed()
 			.setTitle('Member Unanned')
-			.setThumbnail(member.user.displayAvatarURL())
-			.addField('User Unanned', member)
+			.addField('User Unanned', args[0])
 			.addField('unbanned by', message.author)
-			.addField('Reason', reason)
 			.setFooter('Time unbanned', client.user.displayAvatarURL())
 			.setTimestamp();
 
 		message.channel.send(unbanembed);
-
-		try {
-			member.send(`You were unbanned from ${message.guild.name} for ${reason}`);
-		}
-		catch (err) {
-			return;
-		}
 
 	},
 };
